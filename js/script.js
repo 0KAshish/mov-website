4e757966ab24b6ed13600589ecbfb42413d16713
Ashish Maurya <ashishsam.ceh1997@gmail.com>
2024-10-02 12:41:45 +0530

    new card added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const card = document.getElementById("card");
+const card1980 = document.getElementById("card1980");
+
+function nextCard() {
+  console.log("clicked");
+  //   card.style.display = "none";
+  //   card1980.style.display = "block";
+}
